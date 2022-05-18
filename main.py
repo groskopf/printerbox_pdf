3abe50f90481a533859ad2b5fc4a2357f3e1180c
Paul Groskopf <paul@groskopf-embedded.com>
2022-05-18 20:44:20 +0200

    Fix openapi.json issue with UploadFile

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ Created on 19. apr. 2022
 '''
 
 import uvicorn
-from fastapi import FastAPI, UploadFile
+from fastapi import FastAPI, File, UploadFile, Request
 from fastapi.staticfiles import StaticFiles
 from typing import List
 
@@ -21,7 +21,7 @@ app.mount('/images', StaticFiles(directory="images"), name="images")
 app.mount('/printer_queue', StaticFiles(directory="printer_queue"), name="printer_queue")
 
 @app.post('/image_upload')    
-def postImageUpload(file: UploadFile):
+def postImageUpload(file: UploadFile = File(...)):
     return postImage(file)
 
 @app.post('/name_tag')
