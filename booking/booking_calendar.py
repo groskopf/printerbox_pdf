aa5eb4a14fc6baac502b490bbc008360d31d269e
Paul Groskopf <paul@groskopf-embedded.com>
2022-05-21 12:07:58 +0000

    Handle errors

--- booking/booking_calendar.py
+++ booking/booking_calendar.py
@@ -4,50 +4,68 @@ import uuid
 from typing import List
 
 from pydantic import BaseModel
+from fastapi import HTTPException
 from datetime import date as date
 from booking.booking import Booking
 from booking.printer_code import PrinterCode
 
 
 class BookingList(BaseModel):
-    list: List[Booking] = []
+    bookings: List[Booking] = []
 
 
 class BookingCalendar():
 
-    bookings: BookingList
+    calendar: BookingList
 
     def __init__(self):
-        self.bookings = BookingList()
+        self.calendar = BookingList()
+
+    def add(self, startDate: date, endDate: date, printerCode: PrinterCode):
+        # TODO check if it collides with other bookings
+        booking = Booking(startDate, endDate, printerCode,
+                          base64.b64encode(uuid.uuid4().bytes))
+        if(not self.isOverlappingExitingBooking(booking)):
+            self.calendar.bookings.append(booking)
+            return booking
+        raise HTTPException(status_code=404, detail="Item not found")
+
+    def update(self, bookingCode : str, startDate: date, endDate: date, printerCode: PrinterCode):
+        for i in range(len(self.calendar.bookings)):
+            if self.calendar.bookings[i].code == bookingCode:
+                booking = Booking(startDate, endDate, printerCode, bookingCode)
+                self.calendar.bookings[i] = booking
+                return booking
+        raise HTTPException(status_code=404, detail="Item not found")
+
+    def delete(self, bookingCode: str):
+        for i in range(len(self.calendar.bookings)):
+            if self.calendar.bookings[i].code == bookingCode:
+                booking = self.calendar.bookings[i]
+                del self.calendar.bookings[i]
+                return booking
+        raise HTTPException(status_code=404, detail="Item not found")
 
     def save(self, filename: str = 'bookings.json'):
         with open(filename, 'w') as file:
-            file.write(self.bookings.json())
+            file.write(self.calendar.json())
 
     def load(self, filename: str = 'bookings.json'):
         if os.path.exists(filename):
             with open(filename, 'r') as file:
                 rawJson: str = file.read()
                 if rawJson:
-                    self.bookings = BookingList.parse_raw(rawJson)
+                    self.calendar = BookingList.parse_raw(rawJson)
 
-    def add(self, startDate: date, endDate: date, printerCode: PrinterCode):
-        # TODO check if it collides with other bookings
-        booking = Booking(startDate, endDate, printerCode,
-                          base64.b64encode(uuid.uuid4().bytes))
-        self.bookings.list.append(booking)
-        return booking
+    def isOverlappingExitingBooking(self, newBooking: Booking):
+        for existingBooking in self.calendar.bookings:
+            if existingBooking.printerCode == newBooking.printerCode:
+                if (newBooking.startDate <= existingBooking.endDate) and (existingBooking.startDate <= newBooking.endDate):
+                    return True
+        return False
 
     def getBooking(self, bookingCode: str):
-        for booking in self.bookings.list:
+        for booking in self.calendar.bookings:
             if booking.code == bookingCode:
                 return booking
         return None
-
-    def delete(self, bookingCode: str):
-
-        booking = self.getBooking(bookingCode)
-        if booking:
-            self.bookings.list.remove(booking)
-        return booking
-        # self.booking = [        booking for booking in self.bookings.list if not booking == bookingCode]
--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from datetime import date as date
 from calendar import calendar
 import uvicorn
-from fastapi import FastAPI, File, UploadFile
+from fastapi import FastAPI, File, UploadFile, status
 from fastapi.staticfiles import StaticFiles
 from typing import List
 from booking.booking import Booking
@@ -32,33 +32,40 @@ def postImageUpload(file: UploadFile = File(...)):
     return postImage(file)
 
 
-@app.post('/name_tag/{name_tag_type}/{layout}')
+@app.post('/name_tag/{nameTagType}/{layout}', status_code=status.HTTP_201_CREATED)
 def nameTag(printer_code: str, nameTagType: NameTagType, layout: Layout, nameData: NameData):
     return postNameTag(nameTagType, layout, nameData)
 
 
-@app.post('/name_tag_sheet/{name_tag_sheet_type}/{layout}')
+@app.post('/name_tag_sheet/{nameTagSheetType}/{layout}', status_code=status.HTTP_201_CREATED)
 def nameTagSheet(nameTagSheetType: NameTagSheetType, layout: Layout, nameDataList: List[NameData]):
     return postNameTagSheet(nameTagSheetType, layout, nameDataList)
 
 
-@app.post('/booking/add')
-def nameTagSheet(startDate: date, endDate: date, printerCode: PrinterCode):
+@app.post('/bookings')
+def nameTagSheet(startDate: date, endDate: date, printerCode: PrinterCode, status_code=status.HTTP_201_CREATED):
     booking = calendar.add(startDate, endDate, printerCode)
     calendar.save()
     return booking
 
 
-@app.post('/booking/delete')
+@app.put('/bookings/{bookingCode}')
+def nameTagSheet(bookingCode: str, startDate: date, endDate: date, printerCode: PrinterCode):
+    booking = calendar.update(bookingCode, startDate, endDate, printerCode)
+    calendar.save()
+    return booking
+
+
+@app.delete('/bookings/{bookingCode}')
 def nameTagSheet(bookingCode: str):
     booking = calendar.delete(bookingCode)
     calendar.save()
     return booking
 
 
-@app.get('/booking/')
+@app.get('/bookings')
 def nameTagSheet(startDate: str = None, endDate: str = None):
-    return calendar.bookings
+    return calendar.calendar
 
 
 if __name__ == "__main__":
