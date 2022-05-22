bc60ffb11d363cd05657eb45504651f46cc6b297
Paul Groskopf <paul@groskopf-embedded.com>
2022-05-22 08:24:19 +0000

    Test upload

---
+++ test/test_upload.py
@@ -0,0 +1,34 @@
+import os
+from datetime import date as date, timedelta
+from fastapi.testclient import TestClient
+import pytest
+
+from main import app
+from printer_code import PrinterCode
+from test.test_bookings import createBooking, clearBookingList
+
+client = TestClient(app)
+
+@pytest.fixture
+def removeOldImages():
+    for root, dirs, files in os.walk('./images', topdown=False):
+        for name in files:
+            os.remove(os.path.join(root, name))
+        for name in dirs:
+            os.rmdir(os.path.join(root, name))
+
+def uploadImage(imageName : str):
+    with open(imageName, "rb") as f:
+        response = client.post("/upload/images", files={"file": (os.path.basename(imageName), f, "image/jpeg")})
+
+def test_upload_image(removeOldImages):
+    imageList = ['./test/images/logo.jpg', './test/images/Kongresartikler.jpg' ]
+    
+    for imageName in imageList:
+        uploadImage(imageName)
+
+    for imageName in imageList:
+        uploadName = './images/' + os.path.basename(imageName)
+        assert os.path.exists(uploadName)    
+        assert os.path.isfile(uploadName)    
+
