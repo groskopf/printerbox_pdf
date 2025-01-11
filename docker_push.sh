661f24ce33c524128692017537e402776997b450
Paul Groskopf <paul@groskopf-embedded.com>
2025-01-11 12:15:47 +0100

    v1.7

--- docker_push.sh
+++ docker_push.sh
@@ -8,7 +8,7 @@ fi
 echo $CR_PAT | docker login ghcr.io -u USERNAME --password-stdin
 
 image=ghcr.io/groskopf/printerbox_pdf
-tag=1.6
+tag=1.7
 
 docker build -t $image:$tag .
 docker push $image:$tag
