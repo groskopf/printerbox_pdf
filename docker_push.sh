ee94c3d839256392dd60e59caf63e352bc83e95c
Paul Groskopf <paul@groskopf-embedded.com>
2025-09-14 21:07:59 +0200

    Bump docker to 2.1 (#34)

--- docker_push.sh
+++ docker_push.sh
@@ -8,7 +8,7 @@ fi
 echo $CR_PAT | docker login ghcr.io -u USERNAME --password-stdin
 
 image=ghcr.io/groskopf/printerbox_pdf
-tag=1.9
+tag=2.1
 
 docker build -t $image:$tag .
 docker push $image:$tag
